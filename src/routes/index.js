65b7282c22631473938aaee480489a58d34f517d
Bryan <bryan.pilco@gmail.com>
2020-08-02 00:11:15 -0500

    agregando conexion

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-
+const express = require('express');
 const router = express.Router();
+const pool = require('../database');
 
 router.get('/', (req, res) => {
     res.send('Hola chavalines')
